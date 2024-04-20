c9ad19998bd11adb214596635968946b0fbe8f3b
SantiTabbach <tabbach97@gmail.com>
2024-04-20 15:00:01 -0300

    Use Content component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Technologies,
   Typography,
@@ -5,51 +6,57 @@ import {
   Section,
   SocialMedia,
   Header,
+  Content,
 } from "./components";
 import "./App.css";
 
 const { Paragraph, Subtitle } = Typography;
 
+const LeftColumn = () => (
+  <>
+    <Section>
+      <Subtitle fontWeight="100">SSR React Developer</Subtitle>
+      <Paragraph>
+        Currently contributing to the implementation of new functionalities for
+        a USA healthcare project. Simultaneously working on my thesis for
+        software engineering degree.
+      </Paragraph>
+    </Section>
+    <Section title="About me">
+      <Paragraph>
+        Passionate <b>SSR software engineer</b> specialized in frontend
+        development with experience in <b>React</b> and <b>React Native</b>.
+      </Paragraph>
+      <Paragraph>
+        While I love frontend development, I also have a strong interest in and
+        recognize the importance of <b>UX/UI design</b>. Currently, I am honing
+        my technologies in this area using Figma as a design tool and applying
+        design patterns, Nielsen heuristics, Shneiderman's Golden Rules, among
+        others.
+      </Paragraph>
+    </Section>
+    <Section title="Technologies that I use">
+      <Technologies />
+    </Section>
+  </>
+);
+
+const RightColumn = () => (
+  <Section title="Medium posts">
+    <MediumPosts />
+  </Section>
+);
+
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
-      <div className="main-container">
-        <div className="col">
-          <Section>
-            <Subtitle fontWeight="100">SSR React Developer</Subtitle>
-            <Paragraph>
-              Currently contributing to the implementation of new
-              functionalities for a USA healthcare project. Simultaneously
-              working on my thesis for software engineering degree.
-            </Paragraph>
-          </Section>
-          <Section title="About me">
-            <Paragraph>
-              Passionate <b>SSR software engineer</b> specialized in frontend
-              development with experience in <b>React</b> and{" "}
-              <b>React Native</b>.
-            </Paragraph>
-            <Paragraph>
-              While I love frontend development, I also have a strong interest
-              in and recognize the importance of <b>UX/UI design</b>. Currently,
-              I am honing my technologies in this area using Figma as a design
-              tool and applying design patterns, Nielsen heuristics,
-              Shneiderman's Golden Rules, among others.
-            </Paragraph>
-          </Section>
-          <Section title="Technologies that I use">
-            <Technologies />
-          </Section>
-        </div>
-        <div className="col">
-          <Section title="Medium posts">
-            <MediumPosts />
-          </Section>
-        </div>
-      </div>
+      <Content>
+        <LeftColumn />
+        <RightColumn />
+      </Content>
       <SocialMedia />
-    </div>
+    </React.Fragment>
   );
 }
 
