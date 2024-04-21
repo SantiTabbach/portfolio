672d260a86d2d8ab68bbf4a7a9720635d7690dec
SantiTabbach <tabbach97@gmail.com>
2024-04-21 18:40:01 -0300

    New avatars

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,14 +1,24 @@
 import { Typography } from "../../common";
 import Avatar from "../../../assets/avatars/avatar.png";
+import AvatarLight from "../../../assets/avatars/avatar-light.png";
+
 import "./Header.css";
+import { useTheme } from "styled-components";
 
 const { Title } = Typography;
 
 const Header = () => {
+  const theme = useTheme();
+
+  console.log(theme);
+
   return (
     <div className="header-container">
       <div className="santi-tabbach">
-        <img className="avatar" src={Avatar} />
+        <img
+          className="avatar"
+          src={theme.key === "light" ? AvatarLight : Avatar}
+        />
         <div className="name">
           <Title fontWeight="100">SANTI </Title>
           <Title>TABBACH</Title>
