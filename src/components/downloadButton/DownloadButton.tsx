721bd60ed74489379f3c24067eb3b04c0ee2e724
SantiTabbach <tabbach97@gmail.com>
2024-04-27 23:58:14 -0300

    Memoize downloadPdf function

--- src/components/downloadButton/DownloadButton.tsx
+++ src/components/downloadButton/DownloadButton.tsx
@@ -1,19 +1,20 @@
 import DownloadFileIcon from "../../assets/icons/DownloadFileIcon";
 import { StyledTitleLink, StyledButton } from "./StyledDownloadButton";
 import resumePDF from "../../resources/resume.pdf";
+import { useCallback } from "react";
 
 const DownloadButton = () => {
-  const downloadPDF = () => {
+  const downloadPdf = useCallback(() => {
     const link = document.createElement("a");
     link.href = resumePDF;
     link.download = "Santi Tabbach.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  };
+  }, []);
 
   return (
-    <StyledButton onClick={downloadPDF}>
+    <StyledButton onClick={downloadPdf}>
       <StyledTitleLink>Resume</StyledTitleLink>
       <DownloadFileIcon size={20} />
     </StyledButton>
