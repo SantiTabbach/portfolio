9a47fbc99c3550cc39b2bdbbab5a7cfcc73c430a
SantiTabbach <tabbach97@gmail.com>
2024-07-14 18:06:21 -0300

    Try to avoid browser auto translation

--- i18n.ts
+++ i18n.ts
@@ -1,25 +1,34 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en/translation.json';
 import translationES from './locales/es/translation.json';
 
+export const LOCALE_OPTIONS = {
+	EN: 'en',
+	ES: 'es',
+};
+
 const resources = {
-	en: {
+	[LOCALE_OPTIONS.EN]: {
 		translation: translationEN,
 	},
-	es: {
+	[LOCALE_OPTIONS.ES]: {
 		translation: translationES,
 	},
 };
 
-i18n.use(initReactI18next).init({
-	resources,
-	lng: 'en',
-	fallbackLng: 'en',
-	interpolation: {
-		escapeValue: false,
-	},
-});
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources,
+		lng: LOCALE_OPTIONS.EN,
+		fallbackLng: LOCALE_OPTIONS.EN,
+		interpolation: {
+			escapeValue: false,
+		},
+	});
 
 export default i18n;
