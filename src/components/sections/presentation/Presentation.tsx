23c59470b454ec1f848a2659eb34fa08efedc090
SantiTabbach <tabbach97@gmail.com>
2024-07-13 21:12:31 -0300

    Update experience and presentation

--- src/components/sections/presentation/Presentation.tsx
+++ src/components/sections/presentation/Presentation.tsx
@@ -1,25 +1,31 @@
-import { Link, Section, Typography } from "../../common";
-import { StyledBold } from "../../commonStyled";
-import DownloadButton from "../../downloadButton/DownloadButton";
+import { Link, Section, Typography } from '../../common';
+import { StyledBold } from '../../commonStyled';
+import DownloadButton from '../../downloadButton/DownloadButton';
 
 const { Paragraph } = Typography;
 
 const Presentation = () => {
-  return (
-    <Section title="Hey! I'm Santi">
-      <Paragraph>
-        Passionate <StyledBold>SSR Software Developer</StyledBold> with
-        experience in <StyledBold>React</StyledBold> and{" "}
-        <StyledBold>React Native</StyledBold>. Also deeply interested in{" "}
-        <StyledBold>UX/UI design</StyledBold>.
-      </Paragraph>
-      <Paragraph>
-        Currently developing new features for an Argentinian healthcare project
-        at <Link href="https://www.codeait.com/es">CodeaIT</Link>
-      </Paragraph>
-      <DownloadButton />
-    </Section>
-  );
+	return (
+		<Section title="Hey! I'm Santi">
+			<Paragraph>
+				Passionate <StyledBold>SSR Software Developer</StyledBold> with
+				experience in <StyledBold>React</StyledBold> and{' '}
+				<StyledBold>React Native</StyledBold>, and a keen interest in{' '}
+				<StyledBold>UX/UI design</StyledBold>.
+			</Paragraph>
+			<Paragraph>
+				Currently contributing to an exciting web project with cutting-edge
+				technologies, while training in backend development at{' '}
+				<Link href="https://www.codeait.com/es">Codea IT</Link>
+			</Paragraph>
+			<Paragraph>
+				Also working on my{' '}
+				<Link href="https://www.alertify.ar">thesis project</Link> to complete
+				my Software Engineering degree.
+			</Paragraph>
+			<DownloadButton />
+		</Section>
+	);
 };
 
 export default Presentation;
--- src/components/sections/workExperiencie/consts.ts
+++ src/components/sections/workExperiencie/consts.ts
@@ -1,36 +1,48 @@
-import { IWorkExperience } from "../../../models";
+import { IWorkExperience } from '../../../models';
 
 export const WORK_EXPERIENCE: IWorkExperience[] = [
-  {
-    title: "FullStack developer",
-    company: "CodeaIT",
-    time: "2024-06-24 - Present",
-    summary:
-      "Maintaining and developing new features for a mobile PROJECT application and training in backend development.",
-    project: {
-      text: "glucose measurement",
-      url: "https://maverickwellbeing.com/",
-    },
-    skills: ["React Native", "Expo", "Zustand", "Express", "Node", "Mongo"],
-  },
-  {
-    title: "SSR Software Engineer",
-    company: "Intive",
-    time: "2023-08-01 - 2024-06-30",
-    summary:
-      "Maintaining and developing new features for a USA PROJECT desktop application.",
-    project: {
-      text: "shoulder surgery planning",
-      url: "https://shoulderinnovations.com/provoyance-shoulder/",
-    },
-    skills: ["React", "Electron", "Redux", "Jest", "Enzyme", "Axios"],
-  },
-  {
-    title: "Junior Software Engineer",
-    company: "SimTLiX (now Intive)",
-    time: "2021-11-01 - 2023-07-31",
-    summary:
-      "Taking my first steps in my professional career as a developer, learning, and contributing to the team with a proactive attitude.",
-    skills: ["React", "Electron", "Redux", "Jest", "Enzyme", "Axios"],
-  },
+	{
+		title: 'FullStack developer',
+		company: 'Codea IT',
+		time: '2024-06-24 - Present',
+		summary:
+			'Developing new features for an PROJECT web application while taking my first steps in backend development.',
+		project: {
+			text: 'AI-powered',
+			url: 'https://www.emulateme.ai/',
+		},
+		skills: ['React', 'RTKQuery', 'Tailwind', 'Express', 'Node', 'Mongo'],
+	},
+	// {
+	// 	title: 'FullStack developer',
+	// 	company: 'CodeaIT',
+	// 	time: '2024-06-24 - Present',
+	// 	summary:
+	// 		'Maintaining and developing new features for a mobile PROJECT application and training in backend development.',
+	// 	project: {
+	// 		text: 'glucose measurement',
+	// 		url: 'https://maverickwellbeing.com/',
+	// 	},
+	// 	skills: ['React Native', 'Expo', 'Zustand', 'Express', 'Node', 'Mongo'],
+	// },
+	{
+		title: 'SSR Software Engineer',
+		company: 'Intive',
+		time: '2023-08-01 - 2024-06-30',
+		summary:
+			'Maintaining and developing new features for a USA PROJECT desktop application.',
+		project: {
+			text: 'shoulder surgery planning',
+			url: 'https://shoulderinnovations.com/provoyance-shoulder/',
+		},
+		skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
+	},
+	{
+		title: 'Junior Software Engineer',
+		company: 'SimTLiX (now Intive)',
+		time: '2021-11-01 - 2023-07-31',
+		summary:
+			'Taking my first steps in my professional career as a developer, learning, and contributing to the team with a proactive attitude.',
+		skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
+	},
 ];
