795450de668169bc8d783f753ec00821d1009984
SantiTabbach <tabbach97@gmail.com>
2024-08-10 16:58:28 -0300

    BaseList refactor

--- src/components/common/baseList/BaseList.tsx
+++ src/components/common/baseList/BaseList.tsx
@@ -1,23 +1,26 @@
-interface IBaseList<T, P = object & { key: number | string }> {
+import { Key } from 'react';
+
+interface IBaseList<T extends { key?: Key }, P = object> {
 	items: T[];
 	resourceName: string;
 	itemComponent: React.ComponentType<P>;
 }
 
-const BaseList = <T extends object, P>({
+const BaseList = <T extends object & { key?: Key }, P>({
 	items,
 	resourceName,
 	itemComponent: ItemComponent,
 }: IBaseList<T, P>) => {
 	return (
 		<>
-			{items.map((item, index) => (
-				<ItemComponent
-					{...({ [resourceName]: item, key: index } as {
-						key: number | string;
-					} & P)}
-				/>
-			))}
+			{items.map((item, index) => {
+				return (
+					<ItemComponent
+						key={item.key ?? index}
+						{...({ [resourceName]: item } as P)}
+					/>
+				);
+			})}
 		</>
 	);
 };
--- src/models/MediumPost.ts
+++ src/models/MediumPost.ts
@@ -1,7 +1,7 @@
 export type TTopics = string[];
 export type TMediumPost = {
-  url: string;
-  title: string;
-  description: string;
-  topics: TTopics;
+	url: string;
+	title: string;
+	description: string;
+	topics: TTopics;
 };
