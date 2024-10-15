11699c66a8631fcc37fc24cf0aacf5badc9e43ea
SantiTabbach <tabbach97@gmail.com>
2024-10-15 14:54:37 -0300

    Fix experience items

--- src/constants/workExperience.ts
+++ src/constants/workExperience.ts
@@ -2,50 +2,59 @@ import { IWorkExperience } from '@/models';
 
 export const WORK_EXPERIENCE: IWorkExperience[] = [
 	{
-		title: 'work-experience.experience4.title',
 		company: 'Codea IT',
-		time: 'work-experience.experience4.time',
-		summary: 'work-experience.experience4.summary',
-		project: {
-			text: 'work-experience.experience4.project',
-			url: 'https://apps.apple.com/ar/app/maverick-weelbeing/id6502292196?l=en-GB',
-		},
-		skills: [
-			'React Native',
-			'Expo',
-			'Ble PLX',
-			'Zustand',
-			'TanStack Query',
-			'Typescript',
+		experience: [
+			{
+				title: 'work-experience.experience4.title',
+				time: 'work-experience.experience4.time',
+				summary: 'work-experience.experience4.summary',
+				project: {
+					text: 'work-experience.experience4.project',
+					url: 'https://apps.apple.com/ar/app/maverick-weelbeing/id6502292196?l=en-GB',
+				},
+				skills: [
+					'React Native',
+					'Expo',
+					'Ble PLX',
+					'Zustand',
+					'TanStack Query',
+					'Typescript',
+					'NestJS',
+					'Postgres',
+				],
+			},
+			{
+				title: 'work-experience.experience3.title',
+				time: 'work-experience.experience3.time',
+				summary: 'work-experience.experience3.summary',
+				project: {
+					text: 'work-experience.experience3.project',
+					url: 'https://www.emulateme.ai/museums',
+				},
+				skills: ['React', 'RTKQuery', 'Tailwind', 'Express', 'Node', 'Mongo'],
+			},
 		],
 	},
 	{
-		title: 'work-experience.experience3.title',
-		company: 'Codea IT',
-		time: 'work-experience.experience3.time',
-		summary: 'work-experience.experience3.summary',
-		project: {
-			text: 'work-experience.experience3.project',
-			url: 'https://www.emulateme.ai/',
-		},
-		skills: ['React', 'RTKQuery', 'Tailwind', 'Express', 'Node', 'Mongo'],
-	},
-	{
-		title: 'work-experience.experience2.title',
 		company: 'Intive',
-		time: 'work-experience.experience2.time',
-		summary: 'work-experience.experience2.summary',
-		project: {
-			text: 'work-experience.experience2.project',
-			url: 'https://shoulderinnovations.com/provoyance-shoulder/',
-		},
-		skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
-	},
-	{
-		title: 'work-experience.experience1.title',
-		company: 'SimTLiX (now Intive)',
-		time: 'work-experience.experience1.time',
-		summary: 'work-experience.experience1.summary',
-		skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
+		experience: [
+			{
+				title: 'work-experience.experience2.title',
+
+				time: 'work-experience.experience2.time',
+				summary: 'work-experience.experience2.summary',
+				project: {
+					text: 'work-experience.experience2.project',
+					url: 'https://shoulderinnovations.com/provoyance-shoulder/',
+				},
+				skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
+			},
+			{
+				title: 'work-experience.experience1.title',
+				time: 'work-experience.experience1.time',
+				summary: 'work-experience.experience1.summary',
+				skills: ['React', 'Electron', 'Redux', 'Jest', 'Enzyme', 'Axios'],
+			},
+		],
 	},
 ];
